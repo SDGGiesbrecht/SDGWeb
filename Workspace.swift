13d5fc2b683bafe911ec1a8564290067cb192620
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-27 16:46:38 +0200

    Added secure key.

--- Workspace.swift
+++ Workspace.swift
@@ -42,7 +42,7 @@ configuration.documentation.readMe.featureList["🇨🇦EN"] = [
 configuration.documentation.readMe.exampleUsage["🇨🇦EN"] = ""
 
 configuration.continuousIntegration.skipSimulatorOutsideContinuousIntegration = true
-configuration.documentation.api.encryptedTravisCIDeploymentKey = "#warning(No key yet.)"
+configuration.documentation.api.encryptedTravisCIDeploymentKey = "sQc3HlGZyOj3FG45NC9smhjzb34iUQ4KRU95uBtpnXHLs/AcKE5RkED2ljyLJOIv9Wwzb2LIamfORBpbx4QPVgbdqcoeo7CF97+dw3S/xNH4PoLcLHOriS9idSDAqyRL+tOyGyxAPJ94Vu/+Sfn4z6co94Y2ZAaJc8fzYhYA5gdJxwayUve9aP9lw/vKOIOuENtHPrNz6gBbLgM1b9Jp/QTDk+oZ8zpe9l0/rzf4VmYZtI1VGxJGdC3SQKN+TyaJ7UaqCl+G9JdOjuhuRaclNQrKGsP9u+9nqWu9lAN+P3T7TYJnbSMQ+CGGuuXKxxhNKDM4pgtExdpKNLOo5fvISVN4Oi5YSY0KzDx/f0HtXqo4x92tWqWsZzGYjFoInKqKb/4iga3rNOa4vfOMTSbjxHxaQ4KczW+VhznKCKmipBrij8d9zQOsGdrDpVyTkhTcUZPVNjwThs2u0tgVIz4QYGsCqpi536kNzTVZj0v8+G6i1V5y4zlEda09E40XPOizwAZu/qrjAblE2GkL8P1/FADah0ULucQ+mjKZO/afrbmPYCAKDOUqlwUcV+Cd+BOr2ZnGR3TTOZ+mUoyBivPs7ewjTXqt1CwYuia0BgYUqrbupcb895XWeLvRy0mAjjqu4kArMwTiwjeT6rJtdtTW8YzUIqscOtWUcN7zVKkUGKI="
 
 configuration._applySDGOverrides()
 configuration._validateSDGStandards()
