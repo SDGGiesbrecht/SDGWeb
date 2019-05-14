bed658a61ce26b940791b60f5908d384c6f8372a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-14 15:52:27 -0600

    Validated attribute presence.

--- Sources/SDGWeb/AttributeSyntax.swift
+++ Sources/SDGWeb/AttributeSyntax.swift
@@ -12,6 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGLogic
 import SDGCollections
 import SDGLocalization
 
@@ -20,15 +21,17 @@ import SDGHTML
 
 extension AttributeSyntax {
 
+    private static let nonEmptyAttributes: Set<String> = []
+    private static let emptyAttributes: Set<String> = []
+
     private static let urlAttributes: Set<String> = ["href", "src"]
 
     internal func validate(location: String.ScalarView.Index, parentFile: String) -> [SiteValidationError] {
         var result: [SiteValidationError] = []
-        if name.source() ∈ AttributeSyntax.urlAttributes {
-            if let url = value?.value.source() {
+        let name = self.name.source()
 
-            } else {
-                // No value.
+        if name ∈ AttributeSyntax.nonEmptyAttributes {
+            if value == nil {
                 result.append(SiteValidationError.syntaxError(SyntaxError(
                     file: parentFile,
                     index: location,
@@ -40,6 +43,41 @@ extension AttributeSyntax {
 
                     }),
                     context: source())))
+            }
+        } else if name ∈ AttributeSyntax.emptyAttributes {
+            if value ≠ nil {
+                result.append(SiteValidationError.syntaxError(SyntaxError(
+                    file: parentFile,
+                    index: location,
+                    description: UserFacing<StrictString, InterfaceLocalization>({ localization in
+                        switch localization {
+                        case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
+                            return "An attribute has an invalid value."
+                        }
+
+                    }),
+                    context: source())))
+            }
+        } else {
+            result.append(SiteValidationError.syntaxError(SyntaxError(
+                file: parentFile,
+                index: location,
+                description: UserFacing<StrictString, InterfaceLocalization>({ localization in
+                    switch localization {
+                    case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
+                        return "Unknown attribute."
+                    }
+
+                }),
+                context: source())))
+        }
+
+        if name ∈ AttributeSyntax.urlAttributes {
+            if let url = value?.value.source() {
+
+            } else {
+                // No value.
+
             }
         }
         return result
