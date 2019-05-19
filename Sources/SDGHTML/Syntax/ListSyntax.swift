55984bd0b266af221ed4c072e37be3d34ec20e8c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-19 12:21:30 -0600

    Added range replacement to lists.

--- Sources/SDGHTML/Syntax/ListSyntax.swift
+++ Sources/SDGHTML/Syntax/ListSyntax.swift
@@ -12,10 +12,8 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#warning("Add mutators.")
-
 /// A node representing a consecutive list of other nodes.
-public struct ListSyntax<Entry> : RandomAccessCollection, Syntax
+public struct ListSyntax<Entry> : RandomAccessCollection, RangeReplaceableCollection, Syntax
 where Entry : Syntax {
 
     /// Creates a list syntax node from an array of entries.
@@ -56,6 +54,16 @@ where Entry : Syntax {
 
     public func index(_ i: Index, offsetBy distance: Int) -> Index {
         return _storage.children.index(i, offsetBy: distance)
+    }
+
+    // MARK: - RangeReplaceableCollection
+
+    public init() {
+        self.init(entries: [])
+    }
+
+    public mutating func replaceSubrange<S>(_ subrange: Range<Index>, with newElements: S) where S : Sequence, S.Element == Entry {
+        _storage.children.replaceSubrange(subrange, with: newElements.lazy.map({ $0 }))
     }
 
     // MARK: - Syntax
