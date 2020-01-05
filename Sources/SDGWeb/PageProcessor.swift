5abb7dcfad7a33495e135d54a99cf7c6f9474cb6
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-01-05 14:26:57 -0600

    Removed old processing method.

--- Sources/SDGWeb/PageProcessor.swift
+++ Sources/SDGWeb/PageProcessor.swift
@@ -30,24 +30,6 @@ public protocol PageProcessor {
   /// - Parameters:
   ///     - repositoryStructure: The repository structure to get the frame from.
   func frame(repositoryStructure: RepositoryStructure) throws -> DocumentSyntax
-
-  /// Processes the page template, inserting its components into the frame.
-  ///
-  /// - Parameters:
-  ///     - pageTemplate: The template to process.
-  ///     - title: The title of the page.
-  ///     - content: The content of the page.
-  ///     - siteRoot: The relative path from the page to the site root.
-  ///     - localizationRoot: The relative path from the page to the root of its localization.
-  ///     - relativePath: The relative path from the site root to the page.
-  func process(
-    pageTemplate: inout StrictString,
-    title: StrictString,
-    content: StrictString,
-    siteRoot: StrictString,
-    localizationRoot: StrictString,
-    relativePath: StrictString
-  )
 }
 
 extension PageProcessor {
--- Sources/SDGWeb/PageTemplate.swift
+++ Sources/SDGWeb/PageTemplate.swift
@@ -210,15 +210,6 @@ internal class PageTemplate<Localization> where Localization: SDGLocalization.In
       )
     }
 
-    site.pageProcessor.process(
-      pageTemplate: &result,
-      title: title,
-      content: content,
-      siteRoot: siteRoot,
-      localizationRoot: localizationRoot,
-      relativePath: relativePath
-    )
-
     var syntax = try DocumentSyntax.parse(source: String(result)).get()
     try syntax.unfold(with: site.pageProcessor.syntaxUnfolder(localization: localization))
     result = StrictString(syntax.source())
