9f385acd2dc3a82de15c2fa873ba6f5381a2168f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-01-05 23:40:46 -0600

    Refactored.

--- Tests/SDGWebTests/Unfolder.swift
+++ Tests/SDGWebTests/Unfolder.swift
@@ -26,52 +26,56 @@ struct Unfolder: SyntaxUnfolderProtocol {
 
   private let standardUnfolder: SyntaxUnfolder
 
+  // MARK: - Individual Unfolding Operations
+
+  func unfold(frame: inout ElementSyntax) {
+    frame = .page(
+      attributes: frame.attributeDictionary,
+      contents: [
+        .element(
+          .header(contents: [
+            .element(
+              .paragraph(contents: [
+                .element(
+                  .localized([
+                    .element(.english([.text("Header")])),
+                    .element(.deutsch([.text("Kopfzeilen")])),
+                    .element(.עברית([.text("כותרת עליונה")])),
+                    .element(.unknownLanguage([.text("?")]))
+                  ])
+                )
+              ]
+              )
+            )
+          ])
+        ),
+        .element(.division(attributes: ["id": "content"], contents: frame.content)),
+        .element(
+          .footer(contents: [
+            .element(
+              .paragraph(contents: [
+                .element(
+                  .localized([
+                    .element(.english([.text("Footer")])),
+                    .element(.deutsch([.text("Fußzeilen")])),
+                    .element(.עברית([.text("כותרת תחתונה")])),
+                    .element(.unknownLanguage([.text("?")]))
+                  ])
+                )
+              ]
+              )
+            )
+          ])
+        )
+      ]
+    )
+  }
+
   // MARK: - SyntaxUnfolderProtocol
 
   func unfold(element: inout ElementSyntax) throws {
     if element.nameText == "frame" {
-      let frame = element
-
-      element = .page(
-        attributes: frame.attributeDictionary,
-        contents: [
-          .element(
-            .header(contents: [
-              .element(
-                .paragraph(contents: [
-                  .element(
-                    .localized([
-                      .element(.english([.text("Header")])),
-                      .element(.deutsch([.text("Kopfzeilen")])),
-                      .element(.עברית([.text("כותרת עליונה")])),
-                      .element(.unknownLanguage([.text("?")]))
-                    ])
-                  )
-                ]
-                )
-              )
-            ])
-          ),
-          .element(.division(attributes: ["id": "content"], contents: frame.content)),
-          .element(
-            .footer(contents: [
-              .element(
-                .paragraph(contents: [
-                  .element(
-                    .localized([
-                      .element(.english([.text("Footer")])),
-                      .element(.deutsch([.text("Fußzeilen")])),
-                      .element(.עברית([.text("כותרת תחתונה")])),
-                      .element(.unknownLanguage([.text("?")]))
-                    ])
-                  )
-                ]
-                )
-              )
-            ])
-          )
-        ]
-      )
+      unfold(frame: &element)
     }
     try standardUnfolder.unfold(element: &element)
   }
