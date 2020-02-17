3c20df97a4d9e919d71334306f41dd0792b200b0
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-17 17:02:09 -0600

    Attempted debug print.

--- Tests/SDGWebTests/APITests.swift
+++ Tests/SDGWebTests/APITests.swift
@@ -46,10 +46,15 @@ class SDGWebAPITests: TestCase {
   }
 
   func testLocalized() throws {
+    #warning("Debugging.")
+    do {
     for localization in InterfaceLocalization.allCases {
       try LocalizationSetting(orderOfPrecedence: [localization.code]).do {
         try generate(forMock: "Localized", localization: DoubleLocalization.self)
       }
+    }
+    } catch {
+      print("\(error.localizedDescription)\n\(error)")
     }
   }
 
