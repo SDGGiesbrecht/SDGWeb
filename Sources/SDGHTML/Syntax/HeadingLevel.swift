12fb22ece7517d74450fbb355e9130aaad3f8531
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-07 00:10:44 -0600

    Added heading factories.

---
+++ Sources/SDGHTML/Syntax/HeadingLevel.swift
@@ -0,0 +1,35 @@
+/*
+ HeadingLevel.swift
+
+ This source file is part of the SDGWeb open source project.
+ https://sdggiesbrecht.github.io/SDGWeb
+
+ Copyright ©2020 Jeremy David Giesbrecht and the SDGWeb project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+/// The level of a section heading.
+public enum HeadingLevel: CaseIterable {
+
+  // MARK: - Cases
+
+  /// Heading level 1 (`<h2>`).
+  case level1
+  /// Heading level 2 (`<h3>`).
+  case level2
+  /// Heading level 3 (`<h4>`).
+  case level3
+  /// Heading level 4 (`<h5>`).
+  case level4
+  /// Heading level 6 (`<h6>`).
+  case level5
+
+  internal var htmlTag: String {
+    let number: Int = HeadingLevel.allCases.firstIndex(of: self)! + 2
+    return "h\(number)"
+  }
+}
--- Sources/SDGHTML/Syntax/Nodes/ElementSyntax+Factories.swift
+++ Sources/SDGHTML/Syntax/Nodes/ElementSyntax+Factories.swift
@@ -208,6 +208,22 @@ extension ElementSyntax {
     return ElementSyntax(name: "header", attributes: attributes, contents: contents)
   }
 
+  /// Creates a heading (`<h2>`, `<h3>`, etc.).
+  ///
+  /// Heading levels 1 through 5 correspond to tags `<h2>` to `<h3>`. For the `<h1>` tag, use `title(attributes:contents:)` instead.
+  ///
+  /// - Parameters:
+  ///   - level: The nesting level of the heading.
+  ///   - attributes: Optional. The attributes.
+  ///   - contents: The contents of the header.
+  public static func heading(
+    level: HeadingLevel,
+    attributes: [String: String] = [:],
+    contents: ListSyntax<ContentSyntax> = []
+  ) -> ElementSyntax {
+    return ElementSyntax(name: level.htmlTag, attributes: attributes, contents: contents)
+  }
+
   /// Creates a keywords metadata entry.
   ///
   /// - Parameters:
