c352714c2583c7e808c4fee59b12699426df3bba
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-08-12 18:56:42 -0600

    Refreshed.

--- Tests/SDGHTMLTests/WindowsTestPatches.swift
+++ Tests/SDGHTMLTests/WindowsTestPatches.swift
@@ -1,7 +1,23 @@
+/*
+ WindowsTestPatches.swift
+
+ This source file is part of the SDGWeb open source project.
+ https://sdggiesbrecht.github.io/SDGWeb
+
+ Copyright ©2020 Jeremy David Giesbrecht and the SDGWeb project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
 #if os(Windows)
 
+  import SDGText
+
   // #workaround(SDGCornerstone 5.4.1, Requires newline normalization.)
-  public func testFileConvertibleConformance<T>(
+  func testFileConvertibleConformance<T>(
     of instance: T,
     uniqueTestName: StrictString,
     file: StaticString = #file,
