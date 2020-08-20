c281fd134e0538559fbf4186390e53b28879f12b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-08-19 19:29:37 -0600

    Adjusted manifest.

--- Package.swift
+++ Package.swift
@@ -185,9 +185,21 @@ if ProcessInfo.processInfo.environment["TARGETING_WEB"] == "true" {
   }
 }
 
+if ProcessInfo.processInfo.environment["TARGETING_TVOS"] == "true" {
+  // #workaround(xcodebuild -version 11.6, Tool targets don’t work on tvOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.name.hasPrefix("generate") })
+}
+
+if ProcessInfo.processInfo.environment["TARGETING_IOS"] == "true" {
+  // #workaround(xcodebuild -version 11.6, Tool targets don’t work on iOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.name.hasPrefix("generate") })
+}
+
 if ProcessInfo.processInfo.environment["TARGETING_WATCHOS"] == "true" {
   // #workaround(xcodebuild -version 11.6, Test targets don’t work on watchOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.isTest })
+  // #workaround(xcodebuild -version 11.6, Tool targets don’t work on iOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.name.hasPrefix("generate") })
 }
 
 // Windows Tests (Generated automatically by Workspace.)
