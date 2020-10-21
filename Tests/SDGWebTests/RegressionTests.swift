9aff6973842d15cc7201995e874238bbfd614a0a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-10-21 13:27:15 -0600

    Tried debugging.

--- Tests/SDGWebTests/RegressionTests.swift
+++ Tests/SDGWebTests/RegressionTests.swift
@@ -34,6 +34,8 @@ class RegressionTests: TestCase {
         target: URL(fileURLWithPath: "../")
       ).source()
       try redirectFile.save(to: url.appendingPathComponent("Redirect.html"))
+      #warning("Debugging.")
+      return;
       let warnings = Site<InterfaceLocalization, SyntaxUnfolder>.validate(site: url)
       XCTAssert(warnings.isEmpty, "\(warnings)")
     }
