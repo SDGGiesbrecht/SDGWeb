c0c20cf102d6629a7bab287b6d5685ed685ec77b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-06 14:13:05 -0600

    Refreshed.

--- Package.swift
+++ Package.swift
@@ -207,7 +207,7 @@ for target in package.targets {
     // #workaround(Swift 5.3.3, Web lacks Foundation.URLRequest.)
     .define("PLATFORM_LACKS_FOUNDATION_URL_REQUEST", .when(platforms: [.wasi])),
     // #workaround(SDGCornerstone 7.2.4, Web lacks TestCase.)
-    .define("PLATFORM_LACKS_SDG_CORNERSTONE_TEST_CASE", .when(platforms: [.watchOS]))
+    .define("PLATFORM_LACKS_SDG_CORNERSTONE_TEST_CASE", .when(platforms: [.watchOS])),
   ])
 }
 
