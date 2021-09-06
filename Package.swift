6aea02cd5750a648602546a18af880fcb6b740ae
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-05 22:56:27 -0600

    Fixed watchOS test build.

--- Package.swift
+++ Package.swift
@@ -206,6 +206,8 @@ for target in package.targets {
     .define("PLATFORM_LACKS_FOUNDATION_URL_INIT_FILE_URL_WITH_PATH", .when(platforms: [.wasi])),
     // #workaround(Swift 5.3.3, Web lacks Foundation.URLRequest.)
     .define("PLATFORM_LACKS_FOUNDATION_URL_REQUEST", .when(platforms: [.wasi])),
+    // #workaround(SDGCornerstone 7.2.4, Web lacks TestCase.)
+    .define("PLATFORM_LACKS_SDG_CORNERSTONE_TEST_CASE", .when(platforms: [.watchOS]))
   ])
 }
 
