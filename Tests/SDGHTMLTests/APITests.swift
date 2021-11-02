8bc64cce4a24e0e85cc37ee2b2c26fd3a842f012
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-11-02 15:05:43 -0600

    Disabled impossible test.

--- Tests/SDGHTMLTests/APITests.swift
+++ Tests/SDGHTMLTests/APITests.swift
@@ -706,11 +706,13 @@ class APITests: TestCase {
         )
       }
 
-      expectViolation(
-        named: "Dead Remote Link",
-        in: "<a href=\u{22}http://doesnotexist.invalid\u{22}></a>",
-        overwriteSpecificationInsteadOfFailing: false
-      )
+      #if !PLATFORM_LACKS_FOUNDATION_URL_REQUEST
+        expectViolation(
+          named: "Dead Remote Link",
+          in: "<a href=\u{22}http://doesnotexist.invalid\u{22}></a>",
+          overwriteSpecificationInsteadOfFailing: false
+        )
+      #endif
       expectViolation(
         named: "Missing Attribute Value",
         in: "<a href></a>",
