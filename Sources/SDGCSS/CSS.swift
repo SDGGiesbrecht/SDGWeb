16176f47d3e4839e306d7c3660f713e918718a2c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-08-24 16:42:13 -0600

    Debugging Windows...

--- Sources/SDGCSS/CSS.swift
+++ Sources/SDGCSS/CSS.swift
@@ -21,6 +21,8 @@ public enum CSS {
   /// A recommended root CSS file.
   public static let root: StrictString = {
     var result = StrictString(Resources.root)
+    #warning("Debugging...")
+    print("“\(result)”")
     let match = result.firstMatch(
       for: "/*".scalars
         + RepetitionPattern(ConditionalPattern({ _ in true }), consumption: .lazy)
