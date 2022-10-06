0d3c60c5c711ce0abfdba67060379b1173264a53
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-10-05 20:59:04 -0600

    ...

--- Package.swift
+++ Package.swift
@@ -200,8 +200,8 @@ for target in package.targets {
     // @endExample
 
     // Internal‐only:
-    // #warning(Swift 5.6.1, Web cannot handle long literals.)
-    //.define("PLATFORM_SUFFERS_LONG_LITERAL_BUG", .when(platforms: [.wasi])),
+    // #workaround(Swift 5.7, Web cannot handle long literals.)
+    .define("PLATFORM_SUFFERS_LONG_LITERAL_BUG", .when(platforms: [.wasi])),
     // #warning(Swift 5.6.1, Web lacks Foundation.ProcessInfo.)
     //.define("PLATFORM_LACKS_FOUNDATION_PROCESS_INFO", .when(platforms: [.wasi])),
     // #workaround(Swift 5.7, Web lacks Foundation.URL.checkResourceIsReachable().)
