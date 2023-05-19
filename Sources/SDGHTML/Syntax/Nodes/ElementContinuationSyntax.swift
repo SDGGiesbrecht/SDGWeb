360c484e6e79cce72c88d88b6da79bf1ca7ab389
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-18 22:02:37 -0600

    Added missing imports.

--- Sources/SDGHTML/Syntax/Nodes/ElementContinuationSyntax.swift
+++ Sources/SDGHTML/Syntax/Nodes/ElementContinuationSyntax.swift
@@ -12,6 +12,8 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGCollections
+
 /// The content and closing tag of an element.
 public struct ElementContinuationSyntax: ContainerSyntax, Syntax {
 
--- Sources/SDGHTML/Syntax/Nodes/ElementSyntax+Factories.swift
+++ Sources/SDGHTML/Syntax/Nodes/ElementSyntax+Factories.swift
@@ -14,6 +14,7 @@
 
 import Foundation
 
+import SDGCollections
 import SDGLocalization
 
 extension ElementSyntax {
