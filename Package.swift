7ccaa1a2859fd2f0a476fccd197d4ca1d9804a20
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-28 11:40:15 -0600

    Restored Swift 5.7 support.

--- Package.swift
+++ Package.swift
@@ -81,7 +81,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/SDGGiesbrecht/SDGCornerstone",
-      from: Version(10, 0, 0)
+      from: Version(10, 1, 2)
     )
   ],
   targets: [
@@ -209,8 +209,3 @@ for target in package.targets {
     .define("PLATFORM_LACKS_FOUNDATION_NETWORKING", .when(platforms: [.wasi, .android])),
   ])
 }
-
-// #workaround(Swift 5.7.2, Hardware compatibility; tools version does not reflect support.))
-#if compiler(<5.8) && !os(macOS)
-  #error("Swift 5.7 is only supported on macOS, tvOS, iOS and watchOS; elsewhere, please use Swift 5.8 or select an older version of SDGWeb.")
-#endif
